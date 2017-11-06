fed1a44b40f04f9e36c1228f07fa881715e4f42b
felipefdsilva <felipeferreira@poli.ufrj.br>
2017-11-06 00:53:49 -0200

    Giving up of Documentaries (the structure is too different) and showFile function almost done.

--- htmlFileAnalyse.pl
+++ htmlFileAnalyse.pl
@@ -14,15 +14,14 @@ use HTML::FormatText;
 
 use constant OK=>0;
 use constant {FALSE=>0, TRUE=>1};
-
 use constant URL=>'http://www.imdb.com/search/title?view=advanced&sort=num_votes,desc&genres=';
 use constant GENRES=>qw(Action Adventure Animation
                         Biography Comedy Crime
-                        Documentary Drama Family
-                        Fantasy Film-Noir History
-                        Horror Music Musical
-                        Mystery Romance Sci-Fi
-                        Sport Thriller War Western);
+                        Drama Family Fantasy
+                        Film-Noir History Horror
+                        Music Musical Mystery
+                        Romance Sci-Fi Sport
+                        Thriller War Western);
 
 sub getHtmlFiles {
   foreach my $genre (GENRES) {
@@ -71,42 +70,66 @@ sub validateFile {
 
   return (TRUE, "This is a nice and valid file\n");
 }
-
 sub getHtmlInfo {
   my $filename = $_[0];
   my @movie; my @length; my @genres;
   my @IMDBscore; my @Metascore;
   my @synopsis; my @directors;
-  my @stars;
+  my @stars; my $getNextLines = FALSE;
 
   my $niceHtml = HTML::FormatText->format_file
-  ('./Dados/'.$filename, leftmargin => 0, rightmargin => 300);
+  ($filename, leftmargin => 0, rightmargin => 300);
 
   my @niceHtml = split(/\n/, $niceHtml);
 
   foreach (@niceHtml){
     if (/^(?:\d{1,2}\.) ([\S\s]*) (?:\()(\d{4})(?:\))$/){
       push @movie, $1;
-    } elsif (/(?:\d{1,2}) (?:\|) (\d*) (?:min) (?:\|) ((\w+-?,?\s?)+)/){
-      push @length, $1;
-      push @genres, $2;
-    } elsif (/^(\d{1,2}\.?\d)(?:\s(?:\w+\s){12}\S*\sX\s)(\d{2})(?:\s\w+)$/){
-      push @IMDBscore, $1;
-      push @Metascore, $2;
-    } elsif (/^[A-Za-z.\-'0-9:,.();!?]/ && /(\.|\?)$/ && $_ !~ /IMdb.com/i){
-      push @synopsis, $_;
-    } elsif (/(?:Directors?:\s)((?:\w+\.?|\s|,)+)(?:\|\sStars:\s)([\W\w]+)/i){
-      push @directors, $1;
-      push @stars, $2;
+      $getNextLines = TRUE;
+    }
+    if ($getNextLines){
+      if (/(?:(?:\d{1,2}|\w+)\s\|\s)?(\d{1,3})(?:\smin\s\|\s)((\w+-?,?\s?)+)$/){
+        push @length, "Length (min): ".$1;
+        push @genres, "Genres: ".$2;
+      } elsif (/^(\d{1,2}\.?\d)(?:\s(?:\w+\s){12}\S*\sX)/){
+        push @IMDBscore, "IMDB Score: ".$1;
+        if (/(\d{2,3})(?:\sMetascore)$/i){
+          push @Metascore, "Metascore: ".$1;
+        } else {
+          push @Metascore, "Metascore: ??"
+        }
+      } elsif (/^[A-Za-z.\-'"0-9:,.();!?]/ && /(\.|\?|")$/ && $_ !~ /IMdb.com/i){
+        push @synopsis, "Synopsis: ".$_;
+      } elsif (/^(?:Directors?:\s)([\S\s]+)(?:\|\sStars:\s)([\S\s]+)/i){
+        push @directors, "Director(s): ".$1;
+        push @stars, "Stars: ".$2;
+        $getNextLines=FALSE;
+      }
+    }
+  }
+  return \@movie, \@length, \@genres, \@IMDBscore, \@Metascore, \@synopsis, \@directors, \@stars;
+}
+sub showFile {
+  my @info = @_;
+  my $size = @{$info[0]};
+  print "1\n";
+  for (my $movie = 0; $movie < $size; $movie++){
+    for (my $data=0; $data < 8; $data++) {
+      print ${$info[$data]}[$movie],"\n";
+      if ($data == 7 && $movie != $size-1){print "\n\n",$movie+2,"\n";}
     }
   }
 }
-
+#sub removeRepetitions {
+#}
+#sub analyseSearch {
+#}
 {
   #getHtmlFiles;
   #foreach (GENRES){
   #  print ((validateFile($ARGV[0]))[1]);
   #}
-  getHtmlInfo ($ARGV[0]);
+  my @info = getHtmlInfo ($ARGV[0]);
+  showFile (@info);
   exit(OK);
 }
